994e62df4279dc02a234bd02af9b6a7f3199b510
Amal-Amouri7 <amalamouri07@gmail.com>
2024-06-06 14:24:47 +0100

    seance1

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const http = require("http");
+const { connectToMongoDB } = require("./db/BD");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var osRouter = require("./routes/os");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+require("dotenv").config();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
+app.use("/os", osRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -35,7 +36,12 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json("error");
+});
+
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => {
+  connectToMongoDB(), console.log("app is running on port 5000 ,");
 });
 
 module.exports = app;
